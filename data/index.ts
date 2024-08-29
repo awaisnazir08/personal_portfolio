067a9880e1927c2877383ddab166d4d1f733313e
awaisnazir08 <owaisnazir2228@gmail.com>
2024-08-29 19:53:54 +0500

    added grid

---
+++ data/index.ts
@@ -0,0 +1,5 @@
+export const gridItems = [
+    {title: 'Title1', description: 'Description1', id: 1},
+    {title: 'Title1', description: 'Description1', id: 1},
+    {title: 'Title1', description: 'Description1', id: 1},
+]
