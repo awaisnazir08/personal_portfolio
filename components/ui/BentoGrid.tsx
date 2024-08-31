5a015c30f00eff6b02110e7fa28c485aec989445
awaisnazir08 <owaisnazir2228@gmail.com>
2024-08-31 16:49:44 +0500

    work on recent projects, import and add the 3d-pin

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -54,8 +54,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["MilvusDB", "OpenCV", "Scikit-Learn"];
+  const rightLists = ["Pytorch", "Tensorflow", "TensorRT"];
 
   const [copied, setCopied] = useState(false);
 
