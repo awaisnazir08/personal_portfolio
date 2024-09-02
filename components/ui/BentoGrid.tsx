089c13e86c779411c1e4277f0ef2a82ec2236fe7
awaisnazir08 <owaisnazir2228@gmail.com>
2024-09-02 22:31:04 +0500

    fix the rendering styling issues on small devices

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -85,6 +85,9 @@ export const BentoGridItem = ({
         background: "rgb(4,7,29)",
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          minHeight: id === 1 || id === 2 || id === 3 ? '300px' : 'auto' // adjust the height as needed
+          // height: id === 1 ? '100vh' : id === 2 ? '50vh' : id === 3 ? '50vh' : 'auto'
+
       }}
     >
       {/* add img divs */}
@@ -94,7 +97,7 @@ export const BentoGridItem = ({
             <img
               src={img}
               alt={img}
-              className={cn(imgClassName, "object-cover object-center ")}
+              className={cn(imgClassName, "object-cover object-center")}
             />
           )}
         </div>
@@ -106,7 +109,6 @@ export const BentoGridItem = ({
             <img
               src={spareImg}
               alt={spareImg}
-              //   width={220}
               className="object-cover object-center w-full h-full"
             />
           )}
@@ -121,17 +123,17 @@ export const BentoGridItem = ({
         <div
           className={cn(
             titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            "flex flex-col group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 px-5 p-5 lg:p-10"
           )}
         >
           {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
-          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
-            {description}
+          <div className={`font-sans font-extralight md:max-w-28 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10`}>
+            {description} 
           </div>
           {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
           {/* remove mb-2 mt-2 */}
           <div
-            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10 ${id===3 && 'max-w-28'}`}
           >
             {title}
           </div>
@@ -141,7 +143,7 @@ export const BentoGridItem = ({
 
           {/* Tech stack list div */}
           {id === 3 && (
-            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 ">
               {/* tech stack lists */}
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                 {leftLists.map((item, i) => (
