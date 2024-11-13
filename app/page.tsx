0e4942de273b9e7b1741fb57a7dcf83b2bdc042f
awaisnazir08 <owaisnazir2228@gmail.com>
2024-11-13 13:57:10 +0500

    uncomment the modules

--- app/page.tsx
+++ app/page.tsx
@@ -26,11 +26,11 @@ export default function Home() {
         <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
-        {/* <RecentProjects /> */}
+        <RecentProjects />
         {/* <Client /> */}
-        {/* <Experience /> */}
-        {/* <Approach /> */}
-        {/* <Footer /> */}
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
  );
